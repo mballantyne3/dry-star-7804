1dfadf9640609a92c48b708b960e2eb7a8c8c479
Mary Ballantyne <mballantyne777@gmail.com>
2022-09-26 11:42:14 -0600

    started extension story, created hospital spec and controller

--- spec/features/hospitals/show_spec.rb
+++ spec/features/hospitals/show_spec.rb
@@ -1,5 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe 'hospital show page' do
+  before :each do
+    @seattle_grace = Hospital.create!(name: "Seattle Grace")
+    @melodyMD = @seattle_grace.doctors.create!(name: "Melody", specialty: "Surgery", university: "University of Southern California")
+    @derekMD = @seattle_grace.doctors.create!(name: "Derek", specialty: "Orthopedics", university: "Johns Hopkins University")
+    @mary = @melodyMD.patients.create!(name: "Mary", age: 30)
+    @gandalf = @melodyMD.patients.create!(name: "Ben", age: 187)
+    @pippin = @derekMD.patients.create!(name: "Pippin", age: 16)
 
+    visit "/hospital/#{@seattle_grace.id}"
+  end
+
+  it 'has a show page that displays the hospital name and doctors that work at this hospital' do
+    expect(page).to have_content("Welcome to Seattle Grace")
+
+    within("#doctors-#{@melodyMD.id}") do
+      expect(page).to have_content(@melodyMD.name)
+    end
+    within("#doctors-#{@derekMD.id}") do
+      expect(page).to have_content(@derekMD.name)
+    end
+  end
 end
